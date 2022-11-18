2b6c19648539e305dc223a43414e53e3025d710e
Antonio Pires <pires@bitroli.com>
2022-11-18 15:52:59 -0600

    Adding a base class definition for GPIO so we can maybe pick between fake and real in the future

---
+++ gpio/GPIO.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Antonio Jose Pires on 11/18/22.
+//
+
+#include "GPIO.hpp"
---
+++ gpio/GPIO.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Antonio Jose Pires on 11/18/22.
+//
+
+#ifndef KEGERATOR_QT_GPIO_GPIO_HPP_
+#define KEGERATOR_QT_GPIO_GPIO_HPP_
+
+
+
+class GPIO {
+
+};
+
+
+
+#endif //KEGERATOR_QT_GPIO_GPIO_HPP_
