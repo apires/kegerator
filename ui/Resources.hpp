1ae126066476b7b9bd293bff516679fd8c150a02
Antonio Pires <pires@bitroli.com>
2022-12-08 19:27:58 -0600

    Create a namespace and static resources

---
+++ ui/Resources.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Antonio Jose Pires on 12/8/22.
+//
+
+#ifndef KEGERATOR_QT_UI_RESOURCES_HPP_
+#define KEGERATOR_QT_UI_RESOURCES_HPP_
+
+#include <QFont>
+namespace ui {
+namespace R {
+static const QFont EMOJI_FONT("Sans Serif, Apple Color Emoji", 48)
+}
+}
+#endif //KEGERATOR_QT_UI_RESOURCES_HPP_
