2053222cfd794b7d228c8c76f304a42cfaf508e7
Antonio Pires <pires@bitroli.com>
2022-12-17 22:49:20 -0300

    Cleaning up QEMU stuff, fixing up pi3 emulation, cleaning up GPIO CMakeLists

--- Dockerfile
+++ Dockerfile
@@ -3,14 +3,10 @@ FROM arm64v8/alpine:3.16.3 as builder-stage
 RUN apk add alpine-sdk qt6-qtmultimedia-dev musl musl-utils libunwind-dev glog-dev samurai cmake libgpiod-dev
 RUN apk add gdb
 
-#WORKDIR /build
-#COPY . /usr/src/kegerator.qt
-#RUN cmake -GNinja /usr/src/kegerator.qt
-#RUN ninja -j4
+WORKDIR /build
+COPY . /usr/src/kegerator.qt
+RUN cmake -GNinja /usr/src/kegerator.qt
+RUN ninja -j4
 
-#FROM scratch AS export-stage
-#COPY --from=builder-stage /build/kegerator_qt /
-#COPY --from=builder-stage /build/gpio/KegeratorGPIOTest /
-
-#FROM arm64v8/alpine:3.16.3
-
+FROM scratch AS export-stage
+COPY --from=builder-stage /build/kegerator_qt /
---
+++ qemu/alpine/run.sh
@@ -0,0 +1,21 @@
+qemu-system-aarch64 \
+  -m 4G \
+  -cpu cortex-a72 \
+  -M virt \
+  -kernel image/vmlinuz-lts \
+  -initrd image/initramfs-lts \
+  -device virtio-gpu-pci \
+  -hda image/hda.img \
+  -device nec-usb-xhci \
+  -serial stdio \
+  -usb \
+  -device usb-mouse \
+  -device usb-kbd \
+  -device usb-audio \
+  -netdev user,id=mynet,net=192.168.76.0/24,dhcpstart=192.168.76.9 \
+  -device virtio-net-pci,netdev=mynet \
+  -netdev user,id=mynet2,hostfwd=::5555-:22 \
+  -device virtio-net-pci,netdev=mynet2 \
+  -append "rw earlyprintk modules=ext4,virtio_blk quiet loglevel=8 console=ttyAMA0,115200 root=/dev/vda3 rootwait=1"
+
+# set KEGERATOR_SONG_LIST
