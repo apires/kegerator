a85bab2431475f464c9881c4253fd9d07b8be8b1
Antonio Pires <pires@bitroli.com>
2022-12-21 23:30:31 -0300

    Cleaning comments in main

--- main.cpp
+++ main.cpp
@@ -8,16 +8,5 @@ int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
   Kegerator k;
 
-
-//  kegerator->show();
-
-//  auto pins = std::vector<uint32_t>{21};
-//  std::string path = "/dev/gpiochip0";
-//  auto t = std::make_unique<gpio::GPIOLinux>(path, pins);
-//
-//  t->SetPinChangeCallback(21, [&kegerator](auto _pin) {
-//    kegerator->PlayRandomTrack();
-//  });
-
   return QApplication::exec();
 }
